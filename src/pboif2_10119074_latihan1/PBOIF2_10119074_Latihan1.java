f6977857e494173d42cd7755b48f427be8af292c
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 12:33:32 +0700

    Latihan 1

---
+++ src/pboif2_10119074_latihan1/PBOIF2_10119074_Latihan1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan1;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi    : Perintah Menampilkan Value Ke Layar
+ */
+public class PBOIF2_10119074_Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        System.out.println("BIODATA ROCKSTAR"+"\n" + "-----------------------");
+        System.out.println("NAMA\t\t: Rizki Adam Kurniawan");
+        System.out.println("ALAMAT\t\t: Jalan Antapani No 86");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 25 Januari 1991");
+        System.out.println("-----------------------");
+        System.out.println("HOBI\t\t: Renang & Lari");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+    
+}
